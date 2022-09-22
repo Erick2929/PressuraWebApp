4aed7aa684b03b7c4c113e177e39fb0cf574f835
Erick Siller <ErickSillerInv@hotmail.com>
2022-09-21 19:41:57 -0500

    Front end de Vista de mis pacientes terminado

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
-import { NavbarComponent } from '../navbar/navbar.component';
 import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
+
+export interface User {
+  name: string;
+}
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
+
 export class MainComponent implements OnInit {
 
-
-  myControl = new FormControl('');
-  options: string[] = ['One', 'Two', 'Three'];
   panelOpenState = false;
+
+  myControl = new FormControl<string | User>('');
+  options: User[] = [{name: 'Karen'}, {name: 'Eduardo'}, {name: 'Erick'}];
+  filteredOptions!: Observable<User[]>;
+
 
   constructor(
     private router: Router,
@@ -23,6 +31,23 @@ export class MainComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filter(name as string) : this.options.slice();
+      }),
+    );
+  }
+
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
   }
 
   volverLogin(){
@@ -35,6 +60,10 @@ export class MainComponent implements OnInit {
       this.router.navigate(['/login'])
     })
     .catch();
+  }
+  
+  mainView(){
+    this.router.navigate(['/main-app-view']);
   }
 
 }
